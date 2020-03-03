e0b8fdeb3fa143682adb5cb65385747f1a169c1d
davidcasr <david.castroruiz@gmail.com>
2020-03-03 14:52:45 -0500

    I create filters and dynamics routes

--- platzi-exchange/src/router.js
+++ platzi-exchange/src/router.js
@@ -3,11 +3,13 @@ import Router from "vue-router";
 import Home from "@/views/Home";
 import About from "@/views/About";
 import Error from "@/views/Error";
+import CoinDetail from "@/views/CoinDetail";
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
+  
   routes: [
     {
       path: "/",
@@ -18,6 +20,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/coin/:id",
+      name: "coin-detail",
+      component: CoinDetail
     },
     {
       path: "*",
